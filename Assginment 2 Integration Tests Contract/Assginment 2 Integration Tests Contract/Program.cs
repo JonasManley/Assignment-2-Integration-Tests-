a54d8ceb5d012184269e440861d382c9712b5082
JonasManley <54889189+JonasManley@users.noreply.github.com>
2020-03-23 11:43:36 +0100

    Project addded

---
+++ Assginment 2 Integration Tests Contract/Assginment 2 Integration Tests Contract/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assginment_2_Integration_Tests_Contract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
